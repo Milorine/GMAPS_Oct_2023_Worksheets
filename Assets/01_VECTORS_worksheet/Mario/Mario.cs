2c4a37f4616f13e0fa51b6a3f0c9d4e8a68cbc4a
Milorine <140678248+Milorine@users.noreply.github.com>
2023-10-26 09:18:46 +0800

    commit

--- Assets/01_VECTORS_worksheet/Mario/Mario.cs
+++ Assets/01_VECTORS_worksheet/Mario/Mario.cs
@@ -14,12 +14,27 @@ public class Mario : MonoBehaviour
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
+        gravityDir = planet.transform.position - transform.position;
+        moveDir = new Vector3(gravityDir.y, gravityDir.x * -1, 0f);
+        moveDir = moveDir.normalized * -1f;
 
+        rb.AddForce(moveDir * force);
+
+        gravityNorm = gravityDir.normalized;
+        rb.AddForce(gravityNorm * gravityStrength);
+
+        float angle = Vector3.SignedAngle(gravityDir, moveDir, Vector3.forward);
+
+        rb.MoveRotation(Quaternion.Euler(angle, 0f, 0f));
+        
+        DebugExtension.DebugArrow(Vector3.zero, gravityDir, Color.red, 60f);
+        DebugExtension.DebugArrow(Vector3.zero, moveDir, Color.blue, 60f);
+        
     }
 }
 

52365543eb9f04d747edbbf08c3e6ba3bc39a9df
Milorine <140678248+Milorine@users.noreply.github.com>
2023-11-27 10:25:55 +0800

    worksheet 4

---
+++ Assets/04_SIMPLE_COLLISIONS_worksheet/04_SIMPLE_COLLISIONS_worksheet/Pong/Pong No Unity Physics/Scripts/PongBall.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace pool
+{
+    public class PongBall : MonoBehaviour
+    {
+        // Note that the ball can have different X and Y speeds. 
+        //
+        public float speedX = 4.0f;
+        public float speedY = 2.0f;
+
+        // The ball is actually a square Sprite with the image of a circle.
+        // ballTop, etc., are declared to make the HandleBoundaryCollision
+        // code more intuitive (we think in terms of the top, bottom, left
+        // and right of a ball, rather than as offsets from its centre).
+        //
+        private float radius, ballTop, ballBottom, ballLeft, ballRight;
+
+        // We could have these as public properties in the Inspector, but 
+        // they are not used in any other classes, so keep them private &
+        // obtain refernces to the game objects in Start().
+        //
+        private GameObject leftWall, rightWall, topWall, bottomWall;
+
+        // wallOffset is the distance from the center of a wall to its inner edge 
+        // (that the ball collides with). The height of the horizontal (top/bottom)
+        // walls is the same as the width of the vertical (left/right) walls.
+        //
+        // wallOffset is used is to stop the ball from penetrating the walls (see 
+        // your lecture slides).
+        //
+        float wallOffset;
+
+        // References to the paddles, needed to check for collisions with
+        // each paddle, in HandlePaddleCollision().
+        //
+        private GameObject leftPaddle, rightPaddle;
+        private float paddleHeight, paddleWidth;
+
+        // Just for debugging, to draw a line to show the ball's path.
+        // Used in FixedUpdate().
+        //
+        private Vector3 lastpos;
+
+        void Start()
+        {
+            // https://stackoverflow.com/questions/23535304/getting-the-width-of-a-sprite
+            //
+            radius = GetComponent<SpriteRenderer>().sprite.bounds.size.x / 2f;
+
+            leftWall = GameObject.Find("LeftWall");
+            rightWall = GameObject.Find("RightWall");
+            topWall = GameObject.Find("TopWall");
+            bottomWall = GameObject.Find("BottomWall");
+
+            wallOffset = topWall.GetComponent<SpriteRenderer>().sprite.bounds.size.y / 2f;
+
+            leftPaddle = GameObject.Find("LeftPaddle");
+            rightPaddle = GameObject.Find("RightPaddle");
+
+            paddleHeight = leftPaddle.GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+            paddleWidth = leftPaddle.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+
+            lastpos = transform.position;
+        }
+
+        void HandleBoundaryCollision()
+        {
+            float xpos, ypos;
+
+            if (ballRight > rightWall.transform.position.x - wallOffset)
+            {
+                if (speedX > 0f)
+                {
+                    xpos = rightWall.transform.position.x - wallOffset - radius;
+                }
+                else
+                {
+                    xpos = transform.position.x;
+                }
+                transform.position = new Vector3(xpos, transform.position.y, transform.position.z);
+                speedX = -speedX;
+            }
+
+            // Check for collision with left wall
+            if (ballLeft < leftWall.transform.position.x + wallOffset)
+            {
+                if (speedX < 0f)
+                {
+                    xpos = leftWall.transform.position.x + wallOffset + radius;
+                }
+                else
+                {
+                    xpos = transform.position.x;
+                }
+                transform.position = new Vector3(xpos, transform.position.y, transform.position.z);
+                speedX = -speedX;
+            }
+
+            // Check for collision with top wall
+            if (ballTop > topWall.transform.position.y - wallOffset)
+            {
+                ypos = topWall.transform.position.y - wallOffset - radius;
+                transform.position = new Vector3(transform.position.x, ypos, transform.position.z);
+                speedY = -speedY;
+            }
+
+            // Check for collision with bottom wall
+            if (ballBottom < bottomWall.transform.position.y + wallOffset)
+            {
+                ypos = bottomWall.transform.position.y + wallOffset + radius;
+                transform.position = new Vector3(transform.position.x, ypos, transform.position.z);
+                speedY = -speedY;
+            }
+        }
+
+        void HandlePaddleCollision()
+        {
+            if (transform.position.x < leftPaddle.transform.position.x + paddleWidth / 2
+               && transform.position.y < leftPaddle.transform.position.y + paddleHeight / 2
+               && transform.position.y > leftPaddle.transform.position.y - paddleHeight / 2)
+            {
+                transform.position = new Vector3(leftPaddle.transform.position.x + paddleWidth / 2f + radius,
+                                                 transform.position.y,
+                                                 transform.position.z);
+                speedX = -speedX;
+            }
+            // ball right is greater than paddle left
+            if (transform.position.x + radius > rightPaddle.transform.position.x - paddleWidth / 2f
+                // bottom of ball is less than paddle top
+                && transform.position.y - radius < rightPaddle.transform.position.y + paddleHeight / 2f
+                // top of ball is greater than paddle bottom
+                && transform.position.y + radius > rightPaddle.transform.position.y - paddleHeight / 2f)
+            {
+                transform.position = new Vector3(rightPaddle.transform.position.x - (paddleWidth / 2f) - radius,
+                                                 transform.position.y,
+                                                 transform.position.z);
+                speedX = -speedX;
+            }
+        }
+
+        void FixedUpdate()
+        {
+            Debug.DrawLine(lastpos, transform.position, Color.red, 5f);
+            lastpos = transform.position;
+
+            transform.Translate(new Vector2(speedX * Time.deltaTime, 
+                                            speedY * Time.deltaTime));
+
+            // Move this to the top of FixedUpdate and see what happens
+            ballTop = transform.position.y + radius;
+            ballBottom = transform.position.y - radius;
+            ballLeft = transform.position.x - radius;
+            ballRight = transform.position.x + radius;
+            // 
+
+            HandleBoundaryCollision();
+            HandlePaddleCollision();
+        }
+    }
+}
+
---
+++ Assets/04_SIMPLE_COLLISIONS_worksheet/04_SIMPLE_COLLISIONS_worksheet/SimpleCollision/Scripts/Wall2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Collision
+{
+    public class Wall2D : MonoBehaviour
+    {
+        public Transform top;
+        public Transform bottom;
+
+        public Vector2 wallVec {get; private set;}
+        public Vector2 normalVec {get; private set;}
+
+        void Awake()
+        {
+            wallVec = top.position - bottom.position;
+            normalVec = new Vector2(-wallVec.y, wallVec.x).normalized;
+        }
+    }
+}
